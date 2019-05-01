8171e01f56689fe2f4da822d204fb6485b46adb6
Hassan Khan <Hassan@MacBook-Pro-2.local>
2019-05-01 14:49:57 -0400

    First Commit

---
+++ Concentration/ViewController.swift
@@ -0,0 +1,47 @@
+//
+//  ViewController.swift
+//  Concentration
+//
+//  Created by Hassan Khan on 4/28/19.
+//  Copyright © 2019 Hassan Khan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var flipCount = 0 {
+        didSet {
+            flipCountLabel.text = "Flips: \(flipCount)"
+        }
+    }
+    
+    @IBOutlet weak var flipCountLabel: UILabel!
+    
+    @IBOutlet var cardButtons: [UIButton]!
+    
+    var emojiChoices = ["🎃","👻","🎃","👻"]
+    
+    @IBAction func touchCard(_ sender: UIButton) {
+        flipCount += 1
+        
+        // Exclamation point at end: assume this optional is set, grab value
+        // Can also use "if": if this optional is in set state, execute
+        if let cardNumber = cardButtons.firstIndex(of: sender) {
+            flipCard(withEmoji: emojiChoices[cardNumber], on: sender)
+        } else {
+            print("Chosen card not in cardButtons")
+        }
+    }
+    
+    func flipCard(withEmoji emoji: String, on button: UIButton) {
+        if button.currentTitle == emoji {
+            button.setTitle("", for: UIControl.State.normal)
+            button.backgroundColor = #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1)
+        } else {
+            button.setTitle(emoji, for: UIControl.State.normal)
+            button.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        }
+    }
+}
+
